a5a28f4262cd3dbbf9aea4f7b0c778d153169e9b
Leo <leo.r@fr8mate.com>
2014-01-13 14:59:57 -0500

    complies with jshint strict warnings wrt for…in

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ function extend(target) {
     var sources = [].slice.call(arguments, 1);
     sources.forEach(function (source) {
         for (var prop in source) {
+            if (source.hasOwnProperty(prop)) {
                 target[prop] = source[prop];
             }
+        }
     });
     return target;
 }
