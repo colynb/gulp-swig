b01a6260540f292d3a0e8b97ae780e164e7cdbb9
Colyn <colyn.brown@gmail.com>
2019-03-31 10:52:20 -0700

    Long overdue update of dependencies

--- index.js
+++ index.js
@@ -1,79 +1,80 @@
-var es = require('event-stream');
-var swig = require('swig');
-var clone = require('clone');
-var gutil = require('gulp-util');
-var ext = gutil.replaceExtension;
-var PluginError = gutil.PluginError;
-var fs = require('fs');
-var path = require('path');
+var es = require('event-stream')
+var swig = require('swig')
+var clone = require('clone')
+var replaceExt = require('replace-ext')
+var PluginError = require('plugin-error')
+var fs = require('fs')
+var path = require('path')
 
 function extend(target) {
-  'use strict';
-  var sources = [].slice.call(arguments, 1);
+  'use strict'
+  var sources = [].slice.call(arguments, 1)
   sources.forEach(function(source) {
     for (var prop in source) {
       if (source.hasOwnProperty(prop)) {
-        target[prop] = source[prop];
+        target[prop] = source[prop]
       }
     }
-  });
-  return target;
+  })
+  return target
 }
 
 module.exports = function(options) {
-  'use strict';
+  'use strict'
 
-  var opts = options ? clone(options) : {};
-  opts.ext = opts.ext || ".html";
+  var opts = options ? clone(options) : {}
+  opts.ext = opts.ext || '.html'
 
   if (opts.defaults) {
-    swig.setDefaults(opts.defaults);
+    swig.setDefaults(opts.defaults)
   }
 
   if (opts.setup && typeof opts.setup === 'function') {
-    opts.setup(swig);
+    opts.setup(swig)
   }
 
   function gulpswig(file, callback) {
-
-    var data = opts.data || {}, jsonPath;
+    var data = opts.data || {},
+      jsonPath
 
     if (typeof data === 'function') {
-      data = data(file);
+      data = data(file)
     }
 
     if (file.data) {
-      data = extend(file.data, data);
+      data = extend(file.data, data)
     }
 
     if (opts.load_json === true) {
       if (opts.json_path) {
-        jsonPath = path.join(opts.json_path, ext(path.basename(file.path), '.json'));
+        jsonPath = path.join(
+          opts.json_path,
+          replaceExt(path.basename(file.path), '.json')
+        )
       } else {
-        jsonPath = ext(file.path, '.json');
+        jsonPath = replaceExt(file.path, '.json')
       }
 
       // skip error if json file doesn't exist
       try {
-        data = extend(JSON.parse(fs.readFileSync(jsonPath)), data);
+        data = extend(JSON.parse(fs.readFileSync(jsonPath)), data)
       } catch (err) {}
     }
 
     try {
+      var _swig = opts.varControls ? new swig.Swig(opts) : swig
+      var tpl = _swig.compile(String(file.contents), { filename: file.path })
+      var compiled = tpl(data)
 
-      var _swig = opts.varControls ? new swig.Swig(opts) : swig;
-      var tpl = _swig.compile(String(file.contents), {filename: file.path});
-      var compiled = tpl(data);
+      file.path = replaceExt(file.path, opts.ext)
+      file.contents = Buffer.from(compiled)
 
-      file.path = ext(file.path, opts.ext);
-      file.contents = new Buffer(compiled);
-
-      callback(null, file);
+      callback(null, file)
     } catch (err) {
-      callback(new PluginError('gulp-swig', err));
-      callback();
+      callback(new PluginError('gulp-swig', err))
+      callback()
     }
   }
 
-  return es.map(gulpswig);
-};
+  return es.map(gulpswig)
+}
